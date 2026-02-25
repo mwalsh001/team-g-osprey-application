6c39dde09d0c8ac428fef6694433ded6cc9f7d3f
shriy <skulkarni1@wpi.edu>
2026-02-25 17:52:43 -0500

    hash password

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const {ObjectId} = require("mongodb");
 const MongoClient = require('mongodb').MongoClient;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const bcrypt = require ("bcrypt");
 const app = express();
 
 app.use(express.json());
