c9218801078436e685c4803c7677512b7c41dc42
mwalsh001 <matty71604@gmail.com>
2026-03-05 14:52:11 -0500

    Separated compare graphs to compare tab in dashboard - keep filter option only in compare tab. Highlight most common reason for attrition.

--- client/src/components/KpiGraphs/FilterEnrollmentOverTime.jsx
+++ client/src/components/KpiGraphs/FilterEnrollmentOverTime.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import Chart from "https://cdn.jsdelivr.net/npm/chart.js/auto/+esm";
 import {chooseDisplaySchool, chooseFilterRegion} from "../../api/annualBenchmarkingApi.js";
 
