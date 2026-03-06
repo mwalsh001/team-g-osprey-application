07a9cd12eb0234eeabc32a18d3621b0d80a6464a
Matt W <matty71604@gmail.com>
2026-03-05 19:03:53 -0500

    Made separate AttritionSOC component.

---
+++ client/src/components/KpiGraphs/AttritionSOC.jsx
@@ -0,0 +1,5 @@
+import AttritionYOYChart from "./Attrition.jsx";
+
+export default function AttritionSOCChart(props) {
+    return <AttritionYOYChart {...props} attritionCollection="ENROLL_ATTRITION_SOC" />;
+}
